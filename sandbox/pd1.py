e4f20c5f20765a9b6363653f675c22a9869ae8b0
Kai Ming Lee <kaiminglee@outlook.com>
2015-08-07 23:03:03 +0200

    test sandbox

---
+++ sandbox/pd1.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import, division, print_function
+import numpy as np
+import pandas as pd
+from pandas import Series, DataFrame
+
+x = Series(np.random.normal(size=100))
+print(x)
+
